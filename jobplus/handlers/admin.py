3882daa8f18bccaec6e3f11d6013060c560b5770
notfresh <1195056983@qq.com>
2018-04-09 23:53:20 +0800

    modify code

--- jobplus/handlers/admin.py
+++ jobplus/handlers/admin.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, current_app, redirect, url_for, flash
-from simpledu.decorators import admin_required
-from simpledu.models import db,Course, User
-from simpledu.forms import CourseForm, UpdateUserForm, RegisterForm
+from jobplus.decorators import admin_required
+from jobplus.models import db, User
+from jobplus.forms import  UpdateUserForm, RegisterForm
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -12,39 +12,39 @@ def index():
     return render_template('admin/index.html')
 
 
-@admin.route('/courses')
-@admin_required
-def courses():
-    page = request.args.get('page', default=1, type=int)
-    pagination = Course.query.paginate(
-        page=page,
-        per_page=current_app.config['ADMIN_PER_PAGE'],
-        error_out=False
-    )
-    return render_template('admin/courses.html', pg=pagination)
+# @admin.route('/courses')
+# @admin_required
+# def courses():
+#     page = request.args.get('page', default=1, type=int)
+#     pagination = Course.query.paginate(
+#         page=page,
+#         per_page=current_app.config['ADMIN_PER_PAGE'],
+#         error_out=False
+#     )
+#     return render_template('admin/courses.html', pg=pagination)
 
 
-@admin.route('/courses/create', methods=['GET', 'POST'])
-@admin_required
-def create_course():
-    form = CourseForm()
-    if form.validate_on_submit():
-        form.create_course()
-        flash('课程创建成功', 'success')
-        return redirect(url_for('admin.courses'))
-    return render_template('admin/create_course.html', form=form)
-
-
-@admin.route('/courses/<int:course_id>/edit', methods=['GET', 'POST'])
-@admin_required
-def edit_course(course_id):
-    course = Course.query.get_or_404(course_id)
-    form = CourseForm(obj=course)
-    if form.validate_on_submit():
-        form.update_course(course)
-        flash('课程更新成功', 'success')
-        return redirect(url_for('admin.courses'))
-    return render_template('admin/edit_course.html', form=form, course=course)
+# @admin.route('/courses/create', methods=['GET', 'POST'])
+# @admin_required
+# def create_course():
+#     form = CourseForm()
+#     if form.validate_on_submit():
+#         form.create_course()
+#         flash('课程创建成功', 'success')
+#         return redirect(url_for('admin.courses'))
+#     return render_template('admin/create_course.html', form=form)
+#
+#
+# @admin.route('/courses/<int:course_id>/edit', methods=['GET', 'POST'])
+# @admin_required
+# def edit_course(course_id):
+#     course = Course.query.get_or_404(course_id)
+#     form = CourseForm(obj=course)
+#     if form.validate_on_submit():
+#         form.update_course(course)
+#         flash('课程更新成功', 'success')
+#         return redirect(url_for('admin.courses'))
+#     return render_template('admin/edit_course.html', form=form, course=course)
 
 
 
