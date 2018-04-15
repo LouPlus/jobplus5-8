097831ed1165c786550d18940a28f1640c76c6d5
NewCHYS <270620226@qq.com>
2018-04-15 11:13:03 +0800

    fix #9 submit basic directory structure (#19)
    * fix #9 submit basic directory structure
    
    * 需求分析的简单细化

---
+++ jobplus/config.py
@@ -0,0 +1,24 @@
+class BaseConfig(object):
+    INDEX_PER_PAGE = 9
+    SECRET_KEY = '000000'
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    pass
+
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
+
