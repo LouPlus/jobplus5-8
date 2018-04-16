123ca15abb08fbd4637050287959b8ab4a3ce092
becknian <jxz852@case.edu>
2018-04-16 00:36:19 -0400

    fix #12 设计base.html

---
+++ jobplus/models.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class Base(db.Model):
+    __abstract__ = True
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime,
+                        default=datetime.utcnow,
+                        onupdate=datetime.utcnow)
+
+class User(Base, UserMixin):
+    __tablename__ = 'user'
+
+    ROLE_USER = 10
+    ROLE_COMPANY = 20
+    ROLE_ADMIN = 30
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, index=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    _password = db.Column('password', db.String(256), nullable=False)
+    real_name = db.Column(db.String(20))
+    phone = db.Column(db.String(11))
+    work_years = db.Column(db.SmallInteger)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+    job = db.Column(db.String(64))
+    publish_courses = db.relationship('Course')
+
+    def __repr__(self):
+        return '<User:{}>'.format(self.username)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, orig_password):
+        self._password = generate_password_hash(orig_password)
+
+    def check_password(self, password):
+        return check_password_hash(self._password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == self.ROLE_ADMIN
+
+    @property
+    def is_staff(self):
+        return self.role == self.ROLE_STAFF
